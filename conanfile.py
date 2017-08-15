91f91e2d99067afbdf28d8569b93f16155c38834
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:16 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostParameterConan(ConanFile):
     name = "Boost.Parameter"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/parameter"
+    url = "https://github.com/bincrafters/conan-boost-parameter"
+    source_url = "https://github.com/boostorg/parameter"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "parameter"
@@ -22,7 +22,7 @@ class BoostParameterConan(ConanFile):
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
