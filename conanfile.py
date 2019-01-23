e7f78bf63b219f907aa7b275fa93f32acdf4d9c0
Jerry W <jerrywiltse@gmail.com>
2019-01-22 21:57:18 -0500

    use clang 5.0 for header only, add clang 7 to others, implement versions in recipes, conan no longer allows inheriting version, refresh travis badge, use .ci dir, python 3.6 in appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.67.0@bincrafters/testing")
 
 class BoostParameterConan(base.BoostBaseConan):
     name = "boost_parameter"
+    version = "1.67.0"
     url = "https://github.com/bincrafters/conan-boost_parameter"
     lib_short_names = ["parameter"]
     header_only_libs = ["parameter"]
